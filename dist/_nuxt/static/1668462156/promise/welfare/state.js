ff4e9119123352c117c8de1bc76bf2d177112d8f
BaeGayeon <ebgy918@gmail.com>
2022-11-15 06:45:57 +0900

    add: build

--- dist/_nuxt/static/1668462156/promise/welfare/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhybe2022\u002F_nuxt\u002Fstatic\u002F1668462156",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpromise\u002Fwelfare",config:{_app:{basePath:"\u002Fhybe2022\u002F",assetsPath:"\u002Fhybe2022\u002F_nuxt\u002F",cdnURL:a}}}}(null));
