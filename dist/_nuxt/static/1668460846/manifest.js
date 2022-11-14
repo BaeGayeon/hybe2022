b9e6051729c658aac31fc8e7928387f8477cc17a
BaeGayeon <ebgy918@gmail.com>
2022-11-15 06:40:31 +0900

    add: build 폴더 업로드

--- dist/_nuxt/static/1668460846/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fprofile","\u002F","\u002Fstatement","\u002Fpromise\u002Feducation","\u002Fpromise\u002Fwelfare","\u002Fpromise\u002Fall","\u002Fpromise\u002Ffacility","\u002Fpromise\u002Fcommunication","\u002Fpromise\u002Fevent"]})
