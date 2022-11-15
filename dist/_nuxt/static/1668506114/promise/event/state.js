493936c94454f3a6d4aad71d8c6fc109cbf73136
BaeGayeon <ebgy918@gmail.com>
2022-11-15 19:02:50 +0900

    add: build

--- dist/_nuxt/static/1668506114/promise/event/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhybe2022\u002F_nuxt\u002Fstatic\u002F1668506114",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpromise\u002Fevent",config:{_app:{basePath:"\u002Fhybe2022\u002F",assetsPath:"\u002Fhybe2022\u002F_nuxt\u002F",cdnURL:a}}}}(null));
