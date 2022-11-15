97693758e691328e9aec4dbfccccf24de8694925
BaeGayeon <ebgy918@gmail.com>
2022-11-15 22:20:33 +0900

    add: build

--- dist/_nuxt/static/1668518026/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprofile","\u002Fpromise\u002Fcommunication","\u002Fpromise\u002Fevent","\u002Fpromise\u002Fall","\u002Fpromise\u002Fwelfare","\u002Fstatement","\u002Fpromise\u002Ffacility","\u002Fpromise\u002Feducation"]})
