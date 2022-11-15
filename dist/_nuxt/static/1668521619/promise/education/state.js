206ac9dbeb5c9353cd9c8758fa93015c5f255529
BaeGayeon <ebgy918@gmail.com>
2022-11-15 23:13:47 +0900

    add: build

---
+++ dist/_nuxt/static/1668521619/promise/education/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhybe2022\u002F_nuxt\u002Fstatic\u002F1668521619",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpromise\u002Feducation",config:{_app:{basePath:"\u002Fhybe2022\u002F",assetsPath:"\u002Fhybe2022\u002F_nuxt\u002F",cdnURL:a}}}}(null));
