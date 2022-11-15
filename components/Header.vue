21aa3f0cd92f8c95e8e2364352966b1fee055f4f
BaeGayeon <ebgy918@gmail.com>
2022-11-15 18:10:03 +0900

    feat: menu 아이콘 설정

--- components/Header.vue
+++ components/Header.vue
@@ -21,38 +21,97 @@
         <ul>
           <li class="menu menu1">
             <input type="checkbox" id="promisemenu" v-model="promiseview" />
+            <img
+              class="menuicon"
+              src="../assets/images/plus.png"
+              v-if="!promiseview"
+            />
+            <img
+              class="menuicon"
+              src="../assets/images/minus.png"
+              v-if="promiseview"
+            />
             <label for="promisemenu">공약</label>
             <ul>
               <li class="menu submenu submenu1" v-if="promiseview">
-                <nuxt-link to="/promise/facility">시설</nuxt-link>
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
+                <a to="/promise/facility">시설</a>
               </li>
               <li class="menu submenu" v-if="promiseview">
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
                 <nuxt-link to="/promise/communication">소통</nuxt-link>
               </li>
               <li class="menu submenu" v-if="promiseview">
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
                 <nuxt-link to="/promise/welfare">복지</nuxt-link>
               </li>
               <li class="menu submenu" v-if="promiseview">
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
                 <nuxt-link to="/promise/event">행사</nuxt-link>
               </li>
               <li class="menu submenu" v-if="promiseview">
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
                 <nuxt-link to="/promise/education">교육</nuxt-link>
               </li>
               <li class="menu submenu submenue" v-if="promiseview">
+                <img
+                  class="menuicon"
+                  src="../assets/images/minus.png"
+                  v-if="promiseview"
+                />
                 <nuxt-link to="/promise/all">부록(공약집)</nuxt-link>
               </li>
             </ul>
           </li>
           <li class="menu">
+            <img class="menuicon" src="../assets/images/plus.png" />
             <nuxt-link to="/statement">출마의 변</nuxt-link>
           </li>
-          <li class="menu"><nuxt-link to="/profile">후보자 약력</nuxt-link></li>
+          <li class="menu">
+            <img class="menuicon" src="../assets/images/plus.png" />
+            <nuxt-link to="/profile">후보자 약력</nuxt-link>
+          </li>
           <li class="menu">
             <input type="checkbox" id="snsmenu" v-model="snsview" />
+            <img
+              class="menuicon"
+              src="../assets/images/plus.png"
+              v-if="!snsview"
+            />
+            <img
+              class="menuicon"
+              src="../assets/images/minus.png"
+              v-if="snsview"
+            />
             <label for="snsmenu">하이브의 더 많은 이야기</label>
             <ul>
               <a href="https://www.instagram.com/hyup_hy.be_2022">
                 <li class="menu submenu submenu1" v-if="snsview">
+                  <img
+                    class="menuicon"
+                    src="../assets/images/minus.png"
+                    v-if="snsview"
+                  />
                   하이브 인스타그램
                 </li>
               </a>
@@ -60,6 +119,11 @@
                 href="https://www.youtube.com/channel/UC1g63Zw8O8ks6GuLl7mimww"
               >
                 <li class="menu submenu submenue" v-if="snsview">
+                  <img
+                    class="menuicon"
+                    src="../assets/images/minus.png"
+                    v-if="snsview"
+                  />
                   하이브 유튜브
                 </li>
               </a>
@@ -68,7 +132,9 @@
         </ul>
       </div>
     </nav>
-    <div><img class="hello" src="../assets/images/hello.jpg" /></div>
+    <div class="imgbg">
+      <img class="hello" src="../assets/images/hello.jpg" />
+    </div>
   </div>
 </template>
 
@@ -97,18 +163,9 @@ export default {
   background-color: rgb(22, 126, 67);
 }
 
-body {
-  background-color: rgb(22, 126, 67);
+#header ul {
+  padding: 0px;
 }
-
-ul {
-  padding: 0;
-}
-
-#header {
-  background-color: rgb(22, 126, 67);
-}
-
 nav {
   box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
   background-color: #fff;
@@ -159,6 +216,14 @@ a {
   padding-bottom: 0;
 }
 
+#header {
+  background-color: rgb(22, 126, 67);
+}
+
+.imgbg {
+  background-color: rgb(22, 126, 67);
+}
+
 #logo-img {
   width: 140px;
   padding-left: 10px;
@@ -172,6 +237,11 @@ a {
 
 #snsmenu {
   display: none;
+}
+
+.menuicon {
+  width: 12px;
+  margin-right: 7px;
 }
 
 input[id="menuicon"] {
